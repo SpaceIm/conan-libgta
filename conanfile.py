7bb4c4c6281d6d263775e54ffd312f68de0a0901
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2020-02-21 13:51:38 +0100

    fix misspell

--- conanfile.py
+++ conanfile.py
@@ -38,7 +38,7 @@ class LibgtaConan(ConanFile):
         os.rename(self.name + "-" + self.version, self._source_subfolder)
 
     def build(self):
-        tools.replace_in_file(os.path.join(self._source_subfolder, "CMakeLists"),
+        tools.replace_in_file(os.path.join(self._source_subfolder, "CMakeLists.txt"),
                               "${CMAKE_SOURCE_DIR}", "${CMAKE_CURRENT_SOURCE_DIR}")
         cmake = self._configure_cmake()
         cmake.build()
